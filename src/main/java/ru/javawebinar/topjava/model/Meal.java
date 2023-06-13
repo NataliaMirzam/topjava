f0d0b9796a845b80de0b7dc13fe8cff4f81d1e07
Natalia_Sidorova <natalia.alex.st@gmail.com>
2023-06-13 16:48:27 +0300

    HW02

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,35 +1,34 @@
 package ru.javawebinar.topjava.model;
 
+import ru.javawebinar.topjava.web.SecurityUtil;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class Meal {
-    private Integer id;
-
+public class Meal extends AbstractBaseEntity {
+    private Integer userId;
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
+    public Meal(Integer userId, LocalDateTime dateTime, String description, int calories) {
+        this(null, userId, dateTime, description, calories);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+    public Meal(Integer id, Integer userId, LocalDateTime dateTime, String description, int calories) {
+        super(id);
         this.id = id;
+        this.userId = userId;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
+    public Integer getUserId() {
+        return userId;
     }
 
     public LocalDateTime getDateTime() {
@@ -59,10 +58,11 @@ public class Meal {
     @Override
     public String toString() {
         return "Meal{" +
-                "id=" + id +
+                "userId=" + userId +
                 ", dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
+                ", id=" + id +
                 '}';
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,62 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
 
+import static ru.javawebinar.topjava.util.MealsUtil.*;
+import static ru.javawebinar.topjava.web.SecurityUtil.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
 public class MealRestController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+    @Autowired
     private MealService service;
 
+    public Collection<MealTo> getAll() {
+        log.info("getAll");
+        return getTos(service.getAll(authUserId()), authUserCaloriesPerDay());
+    }
+
+    public Collection<MealTo> getAllBetweenTimes(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        log.info("getAllBetweenTimes");
+        return getFilteredTos(service.getAll(authUserId()), authUserCaloriesPerDay(), startDateTime, endDateTime, LocalTime.class);
+    }
+
+    public Collection<MealTo> getAllBetweenDates(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        log.info("getAllBetweenDates");
+        return getFilteredTos(service.getAll(authUserId()), authUserCaloriesPerDay(), startDateTime, endDateTime, LocalDate.class);
+    }
+
+    public Meal get(int id) {
+        log.info("get {}", id);
+        return service.get(id, authUserId());
+    }
+
+    public Meal create(Meal meal) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        return service.create(meal, authUserId());
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id, authUserId());
+    }
+
+    public void update(Meal meal) {
+        log.info("update {} with id={}", meal, meal.getId());
+        service.update(meal, authUserId());
+    }
 }
